41a64a0051a2b43479dfdc676dff4d89f6d9012c
rober-trl <780906@unizar.es>
2021-04-08 11:53:04 +0200

    jue 08 abr 2021 11:52:41 CEST
    carpetas P1 y P2 y ejercicio pruebas Ruby

---
+++ Practica2/hello_word.rb
@@ -0,0 +1,28 @@
+
+
+
+def listFiles x
+    #exec ("ls -l "  + x)
+    query = 'ls -l ' + x
+    resul = `#{query}` #con `#{nombreVariable}` ejecutas y guardas el resultado en una variable
+    puts resul
+end
+#para poner comentarios se ponen asi, print para no poner espacios
+v1 = ARGV[0]
+v2 = ARGV[1]
+for arg in ARGV
+    puts arg
+end
+if ARGV.length == 1
+    puts "SOLOL 1"
+    if File.directory?(ARGV[0]) and Dir.exist?(ARGV[0])
+        puts "is directory"
+    else
+        puts "not a directory"
+    end
+end
+puts "HOLA MUNDO " + v1 +" " 
+puts (3 + 2)
+#s = `ls -l /home/rober`
+#puts(s)
+listFiles '/home/'
