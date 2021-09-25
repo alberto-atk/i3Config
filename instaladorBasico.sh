c30d03e094f5d505edd5157e3ae31c4dfddd83ed
alberto-atk <73551392+alberto-atk@users.noreply.github.com>
2021-09-25 13:09:49 +0200

    Update and rename instaladorBasico to instaladorBasico.sh

---
+++ instaladorBasico.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt upgrade -y
+
+#Instalacion vscode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt install code
+
+#Instalacion chrome
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo apt-get update
+
+sudo apt-get install libappindicator1
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#Instalacion Github Desktop
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux1/GitHubDesktop-linux-2.9.3-linux1.deb
+
+sudo dpkg -i GitHubDesktop-linux-2.9.3-linux1.deb
+
+
