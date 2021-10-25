b8f5851c022266309a84461c5a2536d8a9b70128
alberto-atk <73551392+alberto-atk@users.noreply.github.com>
2021-10-25 19:33:44 +0200

    Update instaladorBasico.sh

--- instaladorBasico.sh
+++ instaladorBasico.sh
@@ -2,11 +2,9 @@ sudo apt update
 sudo apt upgrade -y
 
 #Instalacion vscode
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo apt-get install snapd
 
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-
-sudo apt install code
+sudo snap install code --classic
 
 #Instalacion chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
