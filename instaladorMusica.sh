ec02e9f4e9bfbf79645c123f8c9552231571e575
alberto-atk <73551392+alberto-atk@users.noreply.github.com>
2021-12-21 23:11:44 +0100

    Delete instaladorMusica.sh

--- instaladorMusica.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-#Descargar genymotion, cuenta la de la uni
-wget https://dl.genymotion.com/releases/genymotion-3.2.1/genymotion-3.2.1-linux_x64.bin
-
-#Descargar virtualbox
-sudo apt install virtualbox
-
-#Descargar compilador ARM
-wget https://github.com/m9rco/Genymotion_ARM_Translation/blob/master/package/Genymotion-ARM-Translation_for_8.0.zip
